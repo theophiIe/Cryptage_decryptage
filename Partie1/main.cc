35e95ed2ce5df2e079f8e2be5e1fe1a6120da069
theophiIe <theomolinatti@gmail.com>
2020-11-09 23:42:25 +0100

    Test de la classe ArbreB avec des exemples d'ajout et de déplacement

--- Partie1/main.cc
+++ Partie1/main.cc
@@ -5,7 +5,24 @@
 int main(int argc, char const *argv[])
 {
     Sommet<int> S1(10);
-    ArbreB<int> a();
-    
+    ArbreB<int> a;
+
+    a.ajoutG(10);
+    std::cout << a.getSommet() << std::endl;
+
+    a.ajoutG(20);
+    std::cout << a.getSommet() << std::endl;
+
+    a.ajoutD(5);
+    std::cout << a.getSommet() << std::endl;
+
+    a.remonter();
+    a.ajoutD(8);
+    std::cout << a.getSommet() << std::endl;
+
+    a.remonter();
+    std::cout << a.getSommet() << std::endl;
+    a.deplacementG();
+    std::cout << a.getSommet() << std::endl;
     return 0;
 }
