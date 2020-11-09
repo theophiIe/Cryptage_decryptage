63759f06b607b90cc931f0b6d8ac3fc168540685
theophiIe <theomolinatti@gmail.com>
2020-11-09 20:01:21 +0100

    Début d'implémentation des classes

--- Partie1/ArbreB.hh
+++ Partie1/ArbreB.hh
@@ -1,13 +1,23 @@
 #if !defined(__ARBREB__)
 #define __ARBREB__
 
+    #include "Sommet.hh"
+
+    template<class T>
     class ArbreB
-    {
+    {   
         private:
-            /* data */
+            Sommet<T> *_racine;
+            Sommet<T> *_sCourant;
+            int _nbr_sommet;
+
         public:
-            ArbreB();
-            ~ArbreB();
+            ArbreB() : _racine(nullptr), _sCourant(nullptr), _nbr_sommet(0) {}
+            ArbreB(const ArbreB &arbre) {}
+            ~ArbreB() {}
+
+            void parcours();
+            int getSommet();
     };    
 
 #endif
--- Partie1/Sommet.hh
+++ Partie1/Sommet.hh
@@ -1,13 +1,20 @@
 #if !defined(__SOMMET__)
 #define __SOMMET__
 
+    #include <iostream>
+
+    template<class T>
     class Sommet
     {
         private:
-            /* data */
+            Sommet<T> *_filsG;
+            Sommet<T> *_filsD;
+            T _etiquette;
+            
         public:
-            Sommet();
-            ~Sommet();
+            Sommet() : _filsG(nullptr), _filsD(nullptr) {}
+            Sommet(const T &etiquette) : _filsG(nullptr), _filsD(nullptr), _etiquette(etiquette) {}
+            ~Sommet(){}
     };
  
 #endif
