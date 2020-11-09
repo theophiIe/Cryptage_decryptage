0f18f1267608e2033f5cac77289883b0caa6bb85
theophiIe <theomolinatti@gmail.com>
2020-11-09 20:01:39 +0100

    Test des constructeurs

--- Partie1/main.cc
+++ Partie1/main.cc
@@ -1,7 +1,11 @@
-
+#include <iostream>
+#include "Sommet.hh"
+#include "ArbreB.hh"
 
 int main(int argc, char const *argv[])
 {
+    Sommet<int> S1(10);
+    ArbreB<int> a();
     
     return 0;
 }
