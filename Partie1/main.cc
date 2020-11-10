5be230cae17405b8fa477239796267cd1d7cdbbb
theophiIe <theomolinatti@gmail.com>
2020-11-10 11:23:33 +0100

    Test de deplacement dans l'arbre

--- Partie1/main.cc
+++ Partie1/main.cc
@@ -13,27 +13,24 @@ int main(int argc, char const *argv[])
     a.ajoutG(20);
     std::cout << a.getSommet() << std::endl;
 
-    a.remonter();
+    a.ajoutD(5);
     std::cout << a.getSommet() << std::endl;
 
-    // a.ajoutD(5);
-    // std::cout << a.getSommet() << std::endl;
+    a.remonter_racine();
+    a.ajoutD(8);
+    std::cout << a.getSommet() << std::endl;
 
-    // a.remonter_racine();
-    // a.ajoutD(8);
-    // std::cout << a.getSommet() << std::endl;
-
-    // a.remonter_racine();
-    // std::cout << a.getSommet() << std::endl;
-    // a.deplacementG();
-    // std::cout << a.getSommet() << std::endl;
-    // a.deplacementD();
-    // std::cout << a.getSommet() << std::endl;
+    a.remonter_racine();
+    std::cout << a.getSommet() << std::endl;
+    a.deplacementG();
+    std::cout << a.getSommet() << std::endl;
+    a.deplacementD();
+    std::cout << a.getSommet() << std::endl;
     
-    // a.remonter();
-    // std::cout << a.getSommet() << std::endl;
-    //a.ajoutG(16);
-    //std::cout << a.getSommet() << std::endl;
+    a.remonter();
+    std::cout << a.getSommet() << std::endl;
+    a.ajoutG(16);
+    std::cout << a.getSommet() << std::endl;
 
     return 0;
 }
