e9bd544ef4cdf46d7001fb5f09813ad387899e9d
theophiIe <theomolinatti@gmail.com>
2020-11-13 17:34:54 +0100

    Ajout fichier de test

---
+++ Partie1/test.hh
@@ -0,0 +1,11 @@
+#ifndef DEF_TEST
+#define DEF_TEST
+
+void testParcoursArbre();
+void testSuppresionArbre();
+void testCopieArbre();
+void testFusionArbre();
+void testEtiquette();
+void testDecompositionArbre();
+
+#endif
