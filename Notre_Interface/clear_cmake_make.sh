caeeff3fe335970e4d436dd15e6668965a4de133
Aeerozz <romaingicquiaud@hotmail.fr>
2020-11-17 13:11:45 +0100

    ajout scripts

---
+++ Notre_Interface/clear_cmake_make.sh
@@ -0,0 +1,4 @@
+# Deletion of all cmake files
+
+rm -r CMakeFiles main_autogen
+rm Makefile main CMakeCache.txt cmake_install.cmake 
---
+++ test/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/main_autogen.dir/ParseCache.txt"
+  "main_autogen"
+  "CMakeFiles/main.dir/main_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/main.dir/src/ImageDisplayer.cpp.o"
+  "CMakeFiles/main.dir/src/MainMenu.cpp.o"
+  "CMakeFiles/main.dir/src/MainWindow.cpp.o"
+  "CMakeFiles/main.dir/src/PrenomDisplayer.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
