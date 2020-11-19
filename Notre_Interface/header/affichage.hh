cb2eee7b371ba760b04a065d8a5d9558e7a419f0
theophiIe <theomolinatti@gmail.com>
2020-11-19 12:36:28 +0100

    Ajout du code pour la construc de l'arbre faite par Romain

--- Notre_Interface/header/affichage.hh
+++ Notre_Interface/header/affichage.hh
@@ -5,6 +5,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGridLayout>
 #include <QtGui/QPainter>
+#include "ArbreB.hh"
 #include "bouton.hh"
 
 class Affichage : public QWidget {
@@ -14,6 +15,12 @@ class Affichage : public QWidget {
         Affichage();
         ~Affichage();
         
+        int ecart_feuille = 50;
+        int ecart_ordonnee = 50;
+        int cercle_size = 35;
+        int _racine_x = 600;
+        int _racine_y = 100;
+
         void paintEvent(QPaintEvent *event);
         QSize sizeHint() const;
 };
--- Notre_Interface/src/affichage.cc
+++ Notre_Interface/src/affichage.cc
@@ -14,15 +14,40 @@ void Affichage::paintEvent(QPaintEvent* event) {
     // QRect rect(0, 0, 170, 45);
     // paint.drawText(rect, Qt::AlignCenter, *Bouton::contenu_fichier);
 
-    paint.drawEllipse(100,100,10,10);
-    paint.drawEllipse(300,100,10,10);
+    // paint.drawEllipse(100,100,10,10);
+    // paint.drawEllipse(300,100,10,10);
 
-    paint.drawLine(150,150,260,150);
+    // paint.drawLine(150,150,260,150);
 
-    paint.drawLine(150,150,110,160);
-    paint.drawLine(300,160,260,150);
+    // paint.drawLine(150,150,110,160);
+    // paint.drawLine(300,160,260,150);
 
-    paint.drawLine(110,80,90,90);
+    // paint.drawLine(110,80,90,90);
+
+    ArbreB<int> a;
+    a.ajoutG(8);
+    a << 5;
+    a >> 7;
+    a.remonter_racine();
+    a.ajoutD(6);
+    a.ajoutD(1);
+    a.remonter_racine();
+    a.deplacementG();
+    a.ajoutG(4);
+    int profondeur = a.get_profondeur();
+
+    a.remonter_racine();
+    if (0/*racine null )*/){
+        std::cout << "\t\t\tCHEH\n\n";
+    }
+
+    if (a.deplacementG()) {
+        
+    }
+
+    if (a.deplacementD()) {
+        
+    }
 }
 
 Affichage::~Affichage() {
