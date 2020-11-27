0afbbd2064e7ef7ad3c0b707d650c1424b1d09d5
theophiIe <theomolinatti@gmail.com>
2020-11-27 19:52:07 +0100

    Ajout d'un nouveau constructeur qui prend une occurence et une lettre

--- Partie2/Notre_Interface/header/Sommet.hh
+++ Partie2/Notre_Interface/header/Sommet.hh
@@ -1,20 +1,21 @@
 #if !defined(__SOMMET__)
 #define __SOMMET__
 
-    #include <iostream>
+#include <iostream>
 
-    template <class T> class ArbreB;
+template <class T> class ArbreB;
 
-    template<class T> class Sommet {
-        friend class ArbreB<T>;
+template<class T> class Sommet {
+    friend class ArbreB<T>;
+    
+    private:
+        Sommet<T> *_filsG;
+        Sommet<T> *_filsD;
+        T _etiquette;
+        char _lettre;
         
-        private:
-            Sommet<T> *_filsG;
-            Sommet<T> *_filsD;
-            T _etiquette;
-            
-            Sommet(const T &etiquette) : _filsG(nullptr), _filsD(nullptr), _etiquette(etiquette) {}
-            
-    };
+        Sommet(const T &etiquette) : _filsG(nullptr), _filsD(nullptr), _etiquette(etiquette), _lettre('\0') {}
+        Sommet(const T &etiquette, const char &lettre) : _filsG(nullptr), _filsD(nullptr), _etiquette(etiquette), _lettre(lettre) {}
+};
  
 #endif
