9e05fb068160f36927b8341b55c2086b4a4e70e1
theophiIe <theomolinatti@gmail.com>
2020-12-06 16:18:31 +0100

    Creation de la méthode codage qui prend en param le texte et la map avec les lettres et leur codage et renvoie le texte codé

--- Partie2/Notre_Interface/header/cryptage.hh
+++ Partie2/Notre_Interface/header/cryptage.hh
@@ -9,4 +9,9 @@ std::map<char, int> calcul_occurence(std::string &mon_texte);
 
 ArbreB<int> *creation_racines(std::map<char, int> &map);
 
+template<typename T>
+void fusion_racines(ArbreB<T> *tabArbre);
+
+std::string codage(std::string texte, std::map<char, std::string> &map);
+
 #endif
--- Partie2/Notre_Interface/src/cryptage.cc
+++ Partie2/Notre_Interface/src/cryptage.cc
@@ -4,7 +4,7 @@ std::map<char, int> calcul_occurence(std::string &mon_texte) {
     std::map<char, int> ma_map;
     char lettre;
     
-    for(std::string::size_type i = 0; i < mon_texte.length(); ++i) {
+    for(std::string::size_type i = 0; i < mon_texte.length(); i++) {
         lettre = mon_texte[i];
 
         if(ma_map.insert(std::make_pair(lettre, 1)).second == false) {
@@ -32,7 +32,19 @@ ArbreB<int> *creation_racines(std::map<char, int> &map) {
     return tabArbre;
 }
 
+// A faire 
 template<typename T>
 void fusion_racines(ArbreB<T> *tabArbre) {
 
+}
+
+std::string codage(std::string texte, std::map<char, std::string> &map) {
+    std::string code;
+
+    for(std::string::size_type i = 0; i < texte.length(); i++) {
+        auto code_lettre = map.find(texte[i]);
+        code += code_lettre->second;
+    }
+
+    return code;
 }
