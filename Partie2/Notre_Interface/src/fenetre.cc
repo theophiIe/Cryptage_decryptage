9021393ef536a4904798ab1e5e339c98accf0809
theophiIe <theomolinatti@gmail.com>
2020-12-09 22:36:37 +0100

    Supp du l'implementation inutile dans le layout du dessin

--- Partie2/Notre_Interface/src/fenetre.cc
+++ Partie2/Notre_Interface/src/fenetre.cc
@@ -7,16 +7,15 @@ Fenetre::Fenetre() {
     
     boutons = new Bouton();
     boutons->show();
-    layout->addWidget(boutons, 1, 0);
+    layout->addWidget(boutons, 0, 0);
 
     dessin = new Affichage();
     dessin->show();
-    layout->addWidget(dessin, 0, 0);
 
     zone_scoll = new QScrollArea();
     zone_scoll->setWidget(dessin);
     zone_scoll->show();
-    layout->addWidget(zone_scoll, 1, 1);
+    layout->addWidget(zone_scoll, 0, 1);
 
     setWindowTitle(tr("Projet IN505 : Partie 1"));
     setMinimumSize(500, 500);    
