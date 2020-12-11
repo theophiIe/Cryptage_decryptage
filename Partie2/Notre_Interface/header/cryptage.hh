828a4bcae705300e2c25db887dfe2657c10912c2
Aeerozz <romaingicquiaud@hotmail.fr>
2020-12-11 10:18:25 +0100

    fonction en itératif

--- Partie2/Notre_Interface/header/cryptage.hh
+++ Partie2/Notre_Interface/header/cryptage.hh
@@ -10,6 +10,10 @@ std::map<char, int> calcul_occurence(std::string &mon_texte);
 
 std::vector<ArbreB<int>> creation_racines(std::map<char, int> &map);
 
+int la_plus_petite(std::vector<ArbreB<int>> &vect_arbre);
+
+int la_deuxieme_plus_petite(std::vector<ArbreB<int>> &vec_arbre, int laplusp);
+
 void fusion_racines(std::vector<ArbreB<int>> &vec_arbre);
 
 std::string codage_texte(std::string &texte, std::map<char, std::string> &map);
--- Partie2/Notre_Interface/src/cryptage.cc
+++ Partie2/Notre_Interface/src/cryptage.cc
@@ -30,42 +30,52 @@ std::vector<ArbreB<int>> creation_racines(std::map<char, int> &map) {
     return vec_arbre;
 }
 
-
-
-// Méthode avec le vecteur et pas le tab d'arbre
-void fusion_racines(std::vector<ArbreB<int>> &vec_arbre) {
-    while (vec_arbre.size() != 1) {
-    int j = 0, plus_petite = 0;
-    std::cout<<"Vector size : "<< vec_arbre.size()<<std::endl;
-
+int la_plus_petite(std::vector<ArbreB<int>> &vec_arbre) {
+    int j = 0;
     for (int i = 0; i < vec_arbre.size(); i++) {
         if (vec_arbre[i].getEtiquette() < vec_arbre[j].getEtiquette()) {
-            std::cout<<"Plus petit ! "<< vec_arbre[i].getEtiquette() << vec_arbre[j].getEtiquette()<<std::endl;
+            // std::cout<<"Plus petit ! "<< vec_arbre[i].getEtiquette() << vec_arbre[j].getEtiquette()<<std::endl;
             j = i;           
         }
         std::cout<< vec_arbre[i].getLettre() <<std::endl;        
     }
+    return j;
+}
 
-    plus_petite = j;
-    j = 0;
-
+int la_deuxieme_plus_petite(std::vector<ArbreB<int>> &vec_arbre, int laplusp) {
+    int j = 0;
     for (int i = 0; i < vec_arbre.size(); i++) {
-        if (vec_arbre[i].getEtiquette() <= vec_arbre[j].getEtiquette() && (i != plus_petite)) {
-            std::cout<<"Plus petit ! "<< vec_arbre[i].getEtiquette() << vec_arbre[j].getEtiquette()<<std::endl;
+        if (vec_arbre[i].getEtiquette() <= vec_arbre[j].getEtiquette() && (i != laplusp)) {
+            // std::cout<<"Plus petit ! "<< vec_arbre[i].getEtiquette() << vec_arbre[j].getEtiquette()<<std::endl;
             j = i;  
         }
-        std::cout<< vec_arbre[i].getLettre() << " occu " << vec_arbre[i].getEtiquette() <<std::endl; 
+        // std::cout<< vec_arbre[i].getLettre() << " occu " << vec_arbre[i].getEtiquette() <<std::endl; 
     }
+    return j;
+}
+
+//Méthode avec le vecteur et pas le tab d'arbre
+void fusion_racines(std::vector<ArbreB<int>> &vec_arbre) {
     
-    std::cout<< "les deux valeurs les plus petites sont sur les lettres : " << vec_arbre[plus_petite].getLettre() << " " << vec_arbre[j].getLettre() <<std::endl;
-    vec_arbre[plus_petite] += vec_arbre[j];
-    vec_arbre.erase(vec_arbre.begin() + j);
+    int j = 0, laplusp, ladeuxplusp;
 
-    std::cout<<"New size of vector : "<<vec_arbre.size()<<std::endl;
+    while (vec_arbre.size() != 1) {
 
-    for (int i = 0; i < vec_arbre.size(); i++) {
-        std::cout<< vec_arbre[i].getLettre() << " occu " << vec_arbre[i].getEtiquette() <<std::endl; 
-    }   
+        std::cout<<"Vector size : "<< vec_arbre.size()<<std::endl;
+
+        laplusp = la_plus_petite(vec_arbre);
+        ladeuxplusp = la_deuxieme_plus_petite(vec_arbre, laplusp);
+    
+        std::cout<< "les deux valeurs les plus petites sont sur les lettres : " << vec_arbre[laplusp].getLettre() << " " << vec_arbre[ladeuxplusp].getLettre() <<std::endl;
+    
+        vec_arbre[laplusp] += vec_arbre[ladeuxplusp];
+        vec_arbre.erase(vec_arbre.begin() + ladeuxplusp);
+
+        std::cout<<"New size of vector : "<<vec_arbre.size()<<std::endl;
+
+        for (int i = 0; i < vec_arbre.size(); i++) {
+            std::cout<< vec_arbre[i].getLettre() << " occu " << vec_arbre[i].getEtiquette() <<std::endl; 
+        }   
     } 
 }
 
